a1e6a6869b57dc24fef5d0a8d893df9d94599aee
adrianix1 <adrian.panczyk@wp.pl>
2024-05-02 19:49:31 +0200

    Main loop added

--- text-to-morse.py
+++ text-to-morse.py
@@ -1,4 +1,12 @@
-import morse_dict
+from morse_dict import MORSE_CODE_DICT as MCD
 
 user_input = input("Enter text you want to convert: ")
-print(morse_dict.MORSE_CODE_DICT.keys())
+
+morse_output = []
+for letter in user_input:
+    letter = letter.upper()
+    if letter in MCD:
+        morse_output.append(MCD[letter])
+    else:
+         morse_output.append(letter)
+print(f'Your string in morse code: {morse_output}')
